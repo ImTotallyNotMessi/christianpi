5d6a3a429e46b7ebd6108e532a4899f0a87e0c6a
Christian Hiscock <drbronygamer@gmail.com>
2018-04-05 16:35:52 -0500

    Testing commit from raspberrypi

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public')) 
+//Home Page
+app.get('/', (req, res) => res.sendFile('index.html',{root: '.'}))
+//About Page
+app.get('/about', (req, res) => res.sendFile('about.html',{root: '.'}))
+//Social Page
+app.get('/social', (req, res) => res.sendFile('social.html',{root: '.'}))
+//Meme Page
+app.get('/stfu', (req, res) => res.sendFile('stfu.html',{root: '.'}))
+//Website Proxy
+app.get('/proxy/:secret/:query', (req, res) => {
+    console.log(process.env.SECRET)
+    if(req.params.secret == process.env.SECRET){
+     fetch('https://'+req.params.query)
+      .then(data=>{
+       return data.text()
+     })
+     .then(str=>{
+       res.send(str)
+     })
+      .catch(e=>{
+       console.log(e)
+       res.send(e)
+     })
+    }else{
+      console.log('INTRUDER!')
+     res.sendFile('/keepout.html',{root:'.'})
+    }
+  })  
+
+/*
+
+*/
+
+
+app.listen(3000, () => console.log('Webserver running on port 3000!'))
