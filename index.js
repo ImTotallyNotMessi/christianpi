98b9f1d527b6ae8f7bbaa02ba9294a0952cb06d3
Christian Hiscock <drbronygamer@gmail.com>
2018-04-05 23:01:56 -0400

    PC Specs Route
    Added PC Specifications route.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.get('/about', (req, res) => res.sendFile('about.html',{root: '.'}))
 app.get('/social', (req, res) => res.sendFile('social.html',{root: '.'}))
 //Meme Page
 app.get('/stfu', (req, res) => res.sendFile('stfu.html',{root: '.'}))
+//PC Specs
+app.get('/specs', (req, res) => res.sendFile('CHRISTIAN-PC.html',{root: '.'}))
 //Website Proxy
 app.get('/proxy/:secret/:query', (req, res) => {
     console.log(process.env.SECRET)
@@ -29,10 +31,5 @@ app.get('/proxy/:secret/:query', (req, res) => {
      res.sendFile('/keepout.html',{root:'.'})
     }
   })  
-
-/*
-
-*/
-
 
 app.listen(3000, () => console.log('Webserver running on port 3000!'))
