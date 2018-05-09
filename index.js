900529b19aa186aee00c1a7020a323ba198f64a5
Christian Hiscock <drbronygamer@gmail.com>
2018-05-09 19:00:25 -0400

    Stylesheet Issue Fixed.
    Main update, stylesheets not linking with html fixed.

--- index.js
+++ index.js
@@ -2,36 +2,13 @@ const express = require('express')
 const app = express()
 const fetch = require('isomorphic-fetch')
 
-app.use(express.static('public')) 
-//Home Page
+app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile('index.html',{root:'.'}))
-//About Page
 app.get('/about.html', (req, res) => res.sendFile('about.html',{root:'.'}))
-//Shop Page
 app.get('/shop.html', (req, res) => res.sendFile('shop.html',{root:'.'}))
-//Meme Page
 app.get('/stfu.html', (req, res) => res.sendFile('stfu.html',{root:'.'}))
-//PC Specs
 app.get('/specs.html', (req, res) => res.sendFile('specs.html',{root:'.'}))
-//Website Proxy
-app.get('/proxy/:secret/:query', (req, res) => {
-    console.log(process.env.SECRET)
-    if(req.params.secret == process.env.SECRET){
-     fetch('https://'+req.params.query)
-      .then(data=>{
-       return data.text()
-     })
-      .then(str=>{
-       res.send(str)
-     })
-      .catch(e=>{
-       console.log(e)
-       res.send(e)
-     })
-    }else{
-      console.log('INTRUDER!')
-     res.sendFile('/keepout.html',{root:'.'})
-    }
-  })
-
+app.use(function (req, res, next) {
+  res.status(404).sendFile('404.html',{root:'.'})
+})
 app.listen(3000, () => console.log('Webserver running on port 3000!'))
