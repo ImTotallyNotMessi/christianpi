e6e143f083b04ea977db0ed0d570b9b9838b843d
Christian Hiscock <drbronygamer@gmail.com>
2018-05-30 12:35:58 -0400

    Added Favicon.
    Also cleaned index.html.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const fetch = require('isomorphic-fetch')
 const path = require('path')
+const favicon = require('serve-favicon')
 const app = express()
 
 app.use(express.static('public'))
+app.use(favicon(path.join('public', 'assets', 'favicon.ico')))
+
 app.get('/', (req, res) => res.sendFile('views/index.html',{root:'.'}))
 app.get('/about', (req, res) => res.sendFile('views/about.html',{root:'.'}))
 app.get('/video', (req, res) => res.sendFile('views/video.html',{root:'.'}))
@@ -19,19 +22,20 @@ app.get('/github', (req, res) => {
   res.status(301).redirect('https://github.com/ImTotallyNotMessi')
 })
 app.get('/files/:file(*)', function(req, res, next){
-  var filePath = path.join(__dirname, 'files', req.params.file);
+  var filePath = path.join('files', req.params.file)
 
   res.download(filePath, function (err) {
-    if (!err) return; // file sent
-    if (err && err.status !== 404) return next(err); // non-404 error
-    // file for download not found
-    res.statusCode = 404;
-    res.send('Cant find that file, sorry!');
-  });
-});
+    if (!err) return // file sent
+    if (err && err.status !== 404) return next(err) // non-404 error
+    // File for Dwonload not found
+    res.statusCode = 404
+    res.send('Cant find that file, sorry!')
+  })
+})
 app.use(function (req, res, next) {
   res.status(404).sendFile('views/404.html',{root:'.'})
 })
 
 app.listen(3000, () => console.log('christianpi.tk is now online and running on port 3000!'))
-// app.listen(80, () => console.log('christianpi.tk is now online and running on port 80'))
+/* app.listen(80, 443 () => console.log('christianpi.tk is now online and running on port 80'))
+80 and 443 is the actual port, 3000 for local development */
