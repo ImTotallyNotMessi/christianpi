f011cf6624977f647be534c4fbba0c29ae845291
Christian Hiscock <drbronygamer@gmail.com>
2018-06-15 09:29:56 -0400

    Added pages, changed css
    Added projects page, trying to style it correctly ;-;

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const express = require('express')
 const fetch = require('isomorphic-fetch')
 const path = require('path')
@@ -12,6 +13,7 @@ app.get('/about', (req, res) => res.sendFile('views/about.html',{root:'.'}))
 app.get('/video', (req, res) => res.sendFile('views/video.html',{root:'.'}))
 app.get('/specs', (req, res) => res.sendFile('views/specs.html',{root:'.'}))
 app.get('/downloads', (req, res) => res.sendFile('views/downloads.html',{root:'.'}))
+app.get('/projects', (req, res) => res.sendFile('views/projects.html',{root:'.'}))
 app.get('/files/:file(*)', function(req, res, next){
   var filePath = path.join('files', req.params.file)
 
@@ -23,26 +25,8 @@ app.get('/files/:file(*)', function(req, res, next){
     res.send('Cant find that file, sorry!')
   })
 })
-app.get('/twitch', (req, res) => {
-  res.status(301).redirect('https://twitch.tv/ImTotallyNotMessi')
-})
-app.get('/twitter', (req, res) => {
-  res.status(301).redirect('https://twitter.com/V3RTMessi')
-})
-app.get('/youtube', (req, res) => {
-  res.status(301).redirect('https://youtube.com/ImTotallyNotMessi')
-})
-app.get('/github', (req, res) => {
-  res.status(301).redirect('https://github.com/ImTotallyNotMessi')
-})
-app.get('/instagram', (req, res) => {
-  res.status(301).redirect('https://instagram.com/messi.gg')
-})
-app.get('/snapchat', (req, res) => {
-  res.status(301).redirect('https://snapchat.com/add/imtotallymessi')
-})
 app.use(function (req, res, next) {
   res.status(404).sendFile('views/404.html',{root:'.'})
 })
 
-app.listen(80, () => console.log('christianpi.tk is now online and running on port 80'))
+app.listen(3000, () => console.log('christianpi.tk is now online and running on port 80'))
