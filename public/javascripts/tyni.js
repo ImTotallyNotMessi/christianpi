4d734f7144845b64c0d32aeec0997f6bbf2dcbb3
Christian Hiscock <drbronygamer@gmail.com>
2019-01-17 10:15:10 -0500

    Created URL Shortener

---
+++ public/javascripts/tyni.js
@@ -0,0 +1,21 @@
+function shorten(e){
+	
+	// Prevent the form from submitting data - we need to send it to our own API!
+	e.preventDefault(); 
+
+	// Get data from the form
+	var url = document.querySelector("#url").value;
+	var key = document.querySelector("#key").value;
+
+	// Generate a random path, if none is present
+	if(key == ""){
+		key = Math.random().toString(36).substr(2, 5);
+	}
+
+	// Send the request to our API
+	fetch(`/shorten?url=${url}&key=${key}`);
+	
+
+	// Display the shortened link
+	document.querySelector("h4").innerHTML = `Shortened at 👉 <a href="/${key}">/${key}</a>`;
+}
